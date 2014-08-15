4448347341c2d19a28c62bac064eb861ca11fd41
Henry Stanley <henry@henrystanley.com>
2014-08-15 15:36:35 +0100

    Rewrote self.convert method to iterate over the numerals array and put the returning numerals into a string

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,9 +1,18 @@
 class RomanNumerals
 
+	ROMAN_NUMERALS = [
+		[10, "X"],
+		[5, "V"],
+		[1, "I"]
+	]
+
 	def self.convert(number)
-		return "X" if number == 10
-		return "V" if number == 5
-		"I"
+		a = ""
+		ROMAN_NUMERALS.each do |pair|
+			(number / pair[0]).times { a << pair[1]; number -= pair[0] }
+		end
+		a
 	end
+
 
 end
