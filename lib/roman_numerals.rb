c0fe24c1895f779a17d5c13500588780e4a00883
Henry Stanley <henry@henrystanley.com>
2014-08-15 10:23:53 +0100

    Cleaned up syntax.

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,14 +1,21 @@
 class RomanNumerals
+	ROMAN_NUMERALS = { 	1000 => "M", 
+						900 => "CM",
+						100 => "C", 
+						90 => "XC", 
+						50 => "L", 
+						40 => "XL", 
+						10 => "X", 
+						9 => "IX", 
+						5 => "V", 
+						4 => "IV", 
+						1 => "I"}
 
-	# Define a constant which is set to an array of roman numerals in descending order.
-	ROMAN_NUMERALS = { 1000 => "M", 900 => "CM", 100 => "C", 90 => "XC", 50 => "L", 40 => "XL", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
-
-	def self.convert(number) # here, we're calling a class method on self - which instantiates a RomanNumerals
+	def self.convert(number)
 		string = ""
 		ROMAN_NUMERALS.each do |k, v|
 			(number / k).times {string << v; number -= k}
 		end
 		string.to_s
 	end
-
 end
