abfaa65eacc3a0099adfa20f92efc8dfea825c57
Henry Stanley <henry@henrystanley.com>
2014-08-15 10:02:26 +0100

    Added method to convert 5 to V

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,6 +1,7 @@
 class RomanNumerals
 
-	def self.convert(numbers)
+	def self.convert(number) # here, we're calling a class method on self - which instantiates a RomanNumerals
+		return "V" if number == 5
 		"I"
 	end
 
